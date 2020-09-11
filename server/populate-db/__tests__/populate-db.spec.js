884788493d27d3eb8fc8b536876427b88932d382
blseich <blseich@gmail.com>
2020-09-11 11:37:27 -0400

    first meaningful test for fetch espn data

--- server/populate-db/__tests__/populate-db.spec.js
+++ server/populate-db/__tests__/populate-db.spec.js
@@ -1,9 +1,26 @@
-const { interopDefault } = require("next/dist/next-server/server/load-components");
-
+import fetch from 'node-fetch';
 import populateDb from '../';
 
+jest.mock('node-fetch');
+fetch.mockReturnValue(Promise.resolve({}));
+
 describe('Populate Database', () => {
-    it("should be a function", () => {
-        expect(typeof populateDb).toBe("function");
+    beforeAll(() => {
+        process.env.ESPN_SWID="espn-swid-cookie-value";
+        process.env.ESPN_ESPN_S2="espn-espn-s2-cookie-value";
+    });
+
+    it("should call fetch with espn url and proper header values", async (done) => {
+        await populateDb();
+        expect(fetch).toHaveBeenCalledWith("https://fantasy.espn.com/apis/v3/games/ffl/seasons/2020/segments/0/leagues/1092538?view=modular&view=mTeam", {
+            headers: {
+                cookies: `SWID=${process.env.ESPN_SWID}; espn_s2=${process.env.ESPN_ESPN_S2};`
+            }
+        });
+        done();
+    });
+
+    afterEach(() => {
+        fetch.mockClear();
     });
 });
--- server/populate-db/index.js
+++ server/populate-db/index.js
@@ -1,3 +1,13 @@
-const populateDb = () => {};
+import fetch from 'node-fetch';
+
+const ESPN_TEAMS_URL="https://fantasy.espn.com/apis/v3/games/ffl/seasons/2020/segments/0/leagues/1092538?view=modular&view=mTeam";
+
+const populateDb = async () => {
+    await fetch(ESPN_TEAMS_URL, {
+        headers: {
+            cookies: `SWID=${process.env.ESPN_SWID}; espn_s2=${process.env.ESPN_ESPN_S2};`,
+        },
+    });
+};
 
 export default populateDb;
