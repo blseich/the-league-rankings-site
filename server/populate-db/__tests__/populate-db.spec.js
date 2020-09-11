65525bea0af7db69b55d0e9786710fdf5d3b0caf
blseich <blseich@gmail.com>
2020-09-11 13:15:06 -0400

    make proper call to espn

--- server/populate-db/__tests__/populate-db.spec.js
+++ server/populate-db/__tests__/populate-db.spec.js
@@ -2,10 +2,13 @@ import fetch from 'node-fetch';
 import populateDb from '../';
 
 jest.mock('node-fetch');
-fetch.mockReturnValue(Promise.resolve({}));
 
-describe('Populate Database', () => {
-    beforeAll(() => {
+describe('Fetch Espn Data', () => {
+    beforeEach(() => {
+        fetch.mockReturnValue(Promise.resolve({
+            ok: true,
+            json: () => Promise.resolve('json response data'),
+        }));
         process.env.ESPN_SWID="espn-swid-cookie-value";
         process.env.ESPN_ESPN_S2="espn-espn-s2-cookie-value";
     });
@@ -14,11 +17,24 @@ describe('Populate Database', () => {
         await populateDb();
         expect(fetch).toHaveBeenCalledWith("https://fantasy.espn.com/apis/v3/games/ffl/seasons/2020/segments/0/leagues/1092538?view=modular&view=mTeam", {
             headers: {
-                cookies: `SWID=${process.env.ESPN_SWID}; espn_s2=${process.env.ESPN_ESPN_S2};`
+                cookie: `SWID=${process.env.ESPN_SWID}; espn_s2=${process.env.ESPN_ESPN_S2};`
             }
         });
         done();
     });
+
+    it("should throw an error if response code from fetch is not 200", async (done) => {
+        const RECEIVED_ERR_MESSAGE = "some error message";
+        fetch.mockReturnValue(Promise.resolve({
+            ok: false,
+            json: () => ({
+                messages: [RECEIVED_ERR_MESSAGE]
+            })
+        }));
+        expect(populateDb()).rejects.toEqual(new Error(`Error retrieving ESPN data: ${RECEIVED_ERR_MESSAGE}`));
+        done();
+    });
+
 
     afterEach(() => {
         fetch.mockClear();
--- server/populate-db/index.js
+++ server/populate-db/index.js
@@ -3,11 +3,18 @@ import fetch from 'node-fetch';
 const ESPN_TEAMS_URL="https://fantasy.espn.com/apis/v3/games/ffl/seasons/2020/segments/0/leagues/1092538?view=modular&view=mTeam";
 
 const populateDb = async () => {
-    await fetch(ESPN_TEAMS_URL, {
+    const res = await fetch(ESPN_TEAMS_URL, {
         headers: {
-            cookies: `SWID=${process.env.ESPN_SWID}; espn_s2=${process.env.ESPN_ESPN_S2};`,
+            cookie: `SWID=${process.env.ESPN_SWID}; espn_s2=${process.env.ESPN_ESPN_S2};`,
         },
     });
+
+    if (!res.ok) {
+        const errorResponse = await res.json();
+        throw Error(`Error retrieving ESPN data: ${errorResponse.messages[0]}`);
+    }
+
+    return res.json();
 };
 
 export default populateDb;
